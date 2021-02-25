4031886d55051b5b28dd59d23e9a81f8e0661164
Sourav Sharma <ss4094061@gmail.com>
2021-02-25 22:11:31 +0530

    program for matrix

---
+++ main.cpp
@@ -0,0 +1,21 @@
+// c++ program to read a matrix of size mxn from the keyboard and display the same using function.
+
+#include<iostream>
+using namespace std;
+int main(){
+int a[50][50],n,m,i,j;
+cout<<"Enter value of m and n:"<<endl;
+cin>>m>>n;
+cout<<"Enter elements of the matrix:"<<endl;
+for(i=0;i<m;++i)
+for(j=0;j<n;++j)
+cin>>a[i][j];
+cout<<"The Matrix is:"<<endl;
+for(i=0;i<m;++i)
+{
+for(j=0;j<n;++j)
+cout<<a[i][j]<<" ";
+cout<<endl;
+}
+return 0;
+}
