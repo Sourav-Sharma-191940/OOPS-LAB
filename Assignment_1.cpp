3e7d2887c6881f6548821da3604bda81d49ca04b
Sourav Sharma <ss4094061@gmail.com>
2021-03-14 16:34:17 +0530

    Assignment_1 added

---
+++ Assignment_1.cpp
@@ -0,0 +1,19 @@
+//Assignment - 1: Write a program using recursive function to calculate the factorial of a given number.
+// Also draw the recursion tree for the test valus taken in the program.
+#include <iostream>
+using namespace std;
+int factorial(int n){
+    if (n<1){
+        return 1;
+    }
+        return n*factorial(n-1);
+}
+int main(){
+    int number;
+    double result;
+    cout<<"\nEnter the number for calculate its factorial- ";
+    cin>>number;
+    result = factorial(number);
+    cout<<"The factorial of "<<number<<" is " << result;
+    return 0;
+}
