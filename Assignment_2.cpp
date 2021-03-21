476f172b2bd66dbe237a485a7d843559d6c49aa2
Sourav Sharma <ss4094061@gmail.com>
2021-03-21 21:44:25 +0530

    Assignment_2 added

---
+++ Assignment_2.cpp
@@ -0,0 +1,23 @@
+//Write a program to make the use of inline function.
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+ inline int power(int a, int b=2){
+     double result;
+      result = pow(a, b);
+      return result;
+}
+void display(double result, int x, int y){
+      cout<<"\n So "<<x<< " to the power " <<y<< " is "<<result<<endl;
+}
+int main(){
+      int x, y=2;
+      double answer;
+      cout<<"Enter the base value ";
+      cin>>x;
+      cout<<"Enter the power value ";
+      cin>>y;
+      answer= power(x, y);
+      display(answer, x, y);
+}
