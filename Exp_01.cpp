250bdde15ebc8c65dab05a3014a1350e66c6500f
shubhamkr18 <shubhamkrbgp18@gmail.com>
2021-05-21 14:34:26 +0530

    updated

--- Exp_01.cpp
+++ Exp_01.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 int main(){
 int a[50][50],n,m,i,j;
-cout<<"Enter value of m and n:"<<endl;
+cout<<"Enter value of m and n:"<<endl;  //changes
 cin>>m>>n;
 cout<<"Enter elements of the matrix:"<<endl;
 for(i=0;i<m;++i)
